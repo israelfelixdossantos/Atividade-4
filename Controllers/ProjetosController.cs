d4b181ac68b41de615300d70fc480457dd296145
Israel Felix <grrifs@gmail.com>
2025-10-05 20:49:36 -0300

    Atividade 4 - Criar o restante do CRUD de projetos, testando-o no Insomnia

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -21,5 +21,31 @@ namespace Exo.WebApi.Controllers
         {
             return Ok(_projetoRepository.Listar());
         }
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            return Ok(_projetoRepository.BuscarPorId(id));
+        }
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            _projetoRepository.BuscarPorId(id);
+            _projetoRepository.Atualizar(id, projeto);
+            return StatusCode(204);
+            
+        }
+        [HttpPost]
+        public IActionResult Cadastra(Projeto projeto)
+        {
+            _projetoRepository.Cadastra(projeto);
+            return StatusCode(204);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _projetoRepository.BuscarPorId(id);
+            _projetoRepository.Deletar(id);
+            return StatusCode(204);
+        }
     }
 }
--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -17,7 +17,37 @@ namespace Exo.WebApi.Repositories
         public List<Projeto> Listar()
         {
             return _context.Projetos.ToList();
+        }
+        public Projeto BuscarPorId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+        public void Atualizar(int id, Projeto projetoAtualizado)
+        {
+            var projeto = _context.Projetos.Find(id);           
             
+            projeto.NomeDoProjeto = projetoAtualizado.NomeDoProjeto;
+            projeto.Area = projetoAtualizado.Area;
+            projeto.Status = projetoAtualizado.Status;
+            
+            _context.SaveChanges();
+
+        }
+        public void Cadastra(Projeto projetoAtualizado)
+        {
+            _context.Projetos.Add(projetoAtualizado);
+
+            _context.SaveChanges();
+
+        }
+        public void Deletar(int id)
+        {
+
+            var projetoBuscado = _context.Projetos.Find(id);
+
+            _context.Projetos.Remove(projetoBuscado);
+
+            _context.SaveChanges();
         }
     }
 }
